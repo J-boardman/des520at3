d85724306be0d4e00a84cc6097d97361d04e8bc6
Jayde Boardman <92242612+J-boardman@users.noreply.github.com>
2022-05-16 18:25:06 +1000

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -3,9 +3,9 @@ import renderComponents from "./components.js";
 import renderClassList from "./classes.js"
 
 (async () => {
-  const data = await fetch('../data.json').then(res => res.json());
-  await renderComponents("../html/components/header.html", "header");
-  await renderComponents("../html/components/footer.html", "footer");
+  const data = await fetch('./data.json').then(res => res.json());
+  await renderComponents("./html/components/header.html", "header");
+  await renderComponents("./html/components/footer.html", "footer");
   await renderClassList();
   
   makeHamburger();
