cd6575e374bff17d18ff22ea5942ebbe8f7ff515
Jayde Boardman <92242612+J-boardman@users.noreply.github.com>
2022-05-16 18:31:28 +1000

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -3,7 +3,6 @@ import renderComponents from "./components.js";
 import renderClassList from "./classes.js"
 
 (async () => {
-  const data = await fetch('https://j-boardman.github.io/des520at3/data.json').then(res => res.json());
   await renderComponents("https://j-boardman.github.io/des520at3/html/components/header.html", "header");
   await renderComponents("https://j-boardman.github.io/des520at3/html/components/footer.html", "footer");
   await renderClassList();
